39bb22adf29e9997ea72dc625d6c2d2f2b36c78d
LeoamrosIV <mrlucke11@gmail.com>
2020-11-25 17:13:37 +0100

    Updated ECommerce.cs

--- ECommerce.cs
+++ ECommerce.cs
@@ -13,27 +13,27 @@ namespace ECommerce
         // visibilità - valore di ritorno - NomeMetodo()
         public void Login()
         {
-            Console.WriteLine("You are logged in ..");
+            Console.WriteLine("You are logged in ...");
         }
         public void Purchase()
         {
-            Console.WriteLine("Purchase");
+            Console.WriteLine("Product purchased.");
         }
         public void MyOrders()
         {
-            Console.WriteLine("Orders");
+            Console.WriteLine("These are your orders.");
         }
         public void WishList()
         {
-            Console.WriteLine("Wishlist");
+            Console.WriteLine("This is your wishlist.");
         }
-        public void Cart()
+        public void AddToCart()
         {
-            Console.WriteLine("Cart");
+            Console.WriteLine("Product added to cart!");
         }
         public void Signin()
         {
-            Console.WriteLine("Signin");
+            Console.WriteLine("You are now signed in.");
         }
     }
 }
