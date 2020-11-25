a2ffcb385b2f54f8c0799435721aacd8bb992576
LeoamrosIV <mrlucke11@gmail.com>
2020-11-25 17:56:08 +0100

    Completed exercises, updated Program.cs and ECommerce.cs

--- ECommerce.cs
+++ ECommerce.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace ECommerce
 {
     class Customer
     {
+        // Fields or Attributes
         protected int Id;
         protected string FirstName;
         protected string LastName;
@@ -10,10 +13,23 @@ namespace ECommerce
         protected string Email;
         protected string Password;
         
+        // Properties
+        // getter - setter
+
+        // Constructor
+        public Customer(string firstName, string lastName, string email)
+        {
+            // Called on instantiation
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+        }
+
+        // Methods
         // visibilità - valore di ritorno - NomeMetodo()
         public void Login()
         {
-            Console.WriteLine("You are logged in ...");
+            Console.WriteLine($"Hi {this.FirstName} {this.LastName}, you are logged in.");
         }
         public void CheckOut()
         {
@@ -34,6 +50,10 @@ namespace ECommerce
         public void Signin()
         {
             Console.WriteLine("You are now signed in.");
+        }
+        public static void PrintSomething()
+        {
+            Console.WriteLine("Something");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,11 +7,26 @@ namespace CSharpExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args[0]);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i].ToUpper());
+            }
+
+            // var firstCustomer = new Customer();
+            Customer firstCustomer = new Customer("Francesco", "Bacci", "madcatxxx@gmail.com");
+            firstCustomer.Login();
+
+            Customer secondCustomer = new Customer("Mario", "Rossi", "m.rossi99@hotmail.com");
+            secondCustomer.Login();
+
+            Customer.PrintSomething();
+
+
+            /* Console.WriteLine(args[0]);
             Console.WriteLine(args[1]);
-            //Console.WriteLine("Enter your name:");
-            //string tuoNome = Console.ReadLine();
-            //Console.WriteLine($"Il tuo nome è {tuoNome.ToUpper()}!");
+            Console.WriteLine("Enter your name:");
+            string tuoNome = Console.ReadLine();
+            Console.WriteLine($"Il tuo nome è {tuoNome.ToUpper()}!"); */
         }
     }
 }
